9f88ee33f3ff6cf67c19d9186737f1823f759f4f
Quinton Woodmansee <quinton.woodmansee@joinroot.com>
2019-07-13 17:53:11 -0400

    added react-navigation-props-mapper for better prop passing

--- modules/navigation/src/components/routes.js
+++ modules/navigation/src/components/routes.js
@@ -1,7 +1,11 @@
 import HomeScreen from '@home/src/scenes/home-screen';
+import SongSelection from '@song-selection/src/scenes/song-selection';
 
 export default {
   Home: {
     screen: HomeScreen,
+  },
+  SongSelection: {
+    screen: SongSelection,
   },
 };
